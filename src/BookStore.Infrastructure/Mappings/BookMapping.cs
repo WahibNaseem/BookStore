b2b8c159a2877db6f0e526178d412156eda712fd
Wahib <wahib1900@gmail.com>
2023-01-19 15:14:34 +0800

    Created entities and BookStoreContext

---
+++ src/BookStore.Infrastructure/Mappings/BookMapping.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class BookMapping : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).IsRequired().HasColumnType("varchar(150)");
+
+            builder.Property(x => x.Author).IsRequired().HasColumnType("varchar(150)");
+
+            builder.Property(x => x.Description).IsRequired().HasColumnType("varchar(350)");
+
+            builder.Property(x => x.Value).IsRequired();
+
+            builder.Property(x => x.PublishDate).IsRequired();
+
+            builder.Property(x => x.CategoryId).IsRequired();
+
+            builder.ToTable("Books");
+        }
+    }
+}
---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(50);
+
+            // 1: N
+            builder.HasMany(b => b.Books).WithOne(c => c.Category).HasForeignKey(b => b.CategoryId);
+            
+        }
+    }
+}
